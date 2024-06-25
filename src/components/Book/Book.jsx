1e8211169ae2d0a8ba788d2152e0a720417cd1f4
sinAbhishek <singhabhshek979@gmail.com>
2024-06-25 08:31:33 +0530

    some fix

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import cbeans from "../../data/image/bookbg.png";
 import { BsTelephone } from "react-icons/bs";
 import { GoPerson } from "react-icons/go";
+import { BsCart3 } from "react-icons/bs";
 import "./book.css";
 const Book = () => {
   return (
-    <div className="bG h-80 w-11/12 relative rounded-3xl">
-      <div className=" bg-black opacity-50 absolute top-0 bottom-0 right-0 left-0 rounded-3xl z-0"></div>
+    <div className="bG h-[80vh] w-11/12 relative rounded-3xl">
+      <div className=" bg-black opacity-40 absolute top-0 bottom-0 right-0 left-0 rounded-3xl z-0"></div>
       <div className="absolute top-0 bottom-0 right-0 left-0 z-20 flex justify-center items-center">
         <div className=" flex flex-col   ">
-          <div className=" flex justify-center">
-            <h2 className=" text-white font-bold text-3xl sm:text-4xl">
-              Lets Book Your Table
+          <div className=" flex flex-col justify-center items-center">
+            <h1 className=" mtxt text-5xl text-white">
+              Brewed to Perfection, Baked with Love
+            </h1>
+            <h2 className="stxt text-orange-200 font-medium text-xl sm:text-4xl mt-6">
+              Treat Yourself Today
             </h2>
           </div>
 
-          <div className="bookBar flex h-12 bg-orange-100 px-2 rounded-full items-center mt-8">
-            <div className="flex items-center">
+          <div className="bookBar flex justify-center mt-2">
+            {/* <div className="flex items-center">
               <BsTelephone color="#8a1c15" size={"1.1rem"} />
               <input
                 className=" ml-2 bg-transparent outline-none  "
@@ -31,9 +35,10 @@ const Book = () => {
                 type="text"
                 placeholder="Your name "
               />
-            </div>
-            <button className=" bg-red-900 h-4/5 rounded-full p-2 text-slate-100 font-semibold">
-              Book Table
+            </div> */}
+            <button className=" flex justify-center items-center w-max hover:scale-105 font-normal hover:bg-black hover:text-white transition duration-105 bg-transparent h-4/5 rounded-full px-4 py-2 text-slate-100 border border-white">
+              Shop Now
+              <BsCart3 color="white" size={"1.2rem"} className="mr-2" />
             </button>
           </div>
         </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,43 +5,50 @@ import { NavLink } from "react-router-dom";
 const Footer = () => {
   return (
     <div
-      style={{ background: "#210c03" }}
+      style={{ background: "black" }}
       className="mt-8 w-screen h-52 flex flex-col"
     >
-      <div className=" flex justify-between items-center h-4/5">
-        <div className="footL w-1/3 flex flex-col justify-center items-center">
-          <p className=" text-2xl font-bold text-white">Follow Us</p>
-          <div className="flex mt-8">
-            <BsTwitter className=" mr-4" size={"1.5rem"} color="#f5c7b5" />
-            <BsInstagram className=" mr-4" size={"1.5rem"} color="#f5c7b5" />
-            <BsFacebook size={"1.5rem"} color="#f5c7b5" />
-          </div>
-        </div>
-        <div className="w-1/3 flex justify-center items-center">
+      <div className=" flex justify-between items-center h-4/5 pt-8">
+        <div className="w-1/3 h-full flex justify-center  ">
           {" "}
           <span className="midlog">Cafe Bites</span>
         </div>
-        <div className="footR w-1/3 flex flex-col justify-center items-center">
-          <p className=" text-2xl font-bold text-white">Links</p>
-          <div className="flex mt-8 flex-col">
-            <NavLink to={"/"} className={" no-underline text-slate-100"}>
-              Home
-            </NavLink>
-            <NavLink className={" no-underline text-slate-100"} to={"/menu"}>
-              Shop
-            </NavLink>
+        <div className="footL w-1/3 flex h-full flex-col  items-center">
+          <div className=" follow w-max flex flex-col ">
+            <p className="  text-2xl font-bold text-white">Follow Us</p>
+            <div className="flex mt-6 text-[20px] md:text-[24px]">
+              <BsTwitter className=" mr-4" color="#f5c7b5" />
+              <BsInstagram className=" mr-4" color="#f5c7b5" />
+              <BsFacebook color="#f5c7b5" />
+            </div>
+          </div>
+        </div>
+
+        <div className="footR w-1/3 h-full flex flex-col  items-center">
+          <div className=" links w-max flex flex-col ">
+            <p className=" text-2xl font-bold text-white">Links</p>
+            <div className="linktypes flex mt-6 flex-col">
+              <NavLink to={"/"} className={" no-underline text-slate-100"}>
+                Home
+              </NavLink>
+              <NavLink className={" no-underline text-slate-100"} to={"/menu"}>
+                Shop
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
       <hr className="" />
       <div className=" flex justify-between h-1/5">
-        <div className="flex w-1/2 justify-center items-center h-full">
+        <div className=" copyright w-1/2 flex justify-center items-center">
+          <p className=" text-sm text-slate-200">
+            Copyright 2023 Cafe Bites,Inc. All rights reserved
+          </p>
+        </div>
+        <div className="tc flex w-1/2 justify-center items-center h-full">
           <p className=" text-sm text-slate-200 mr-2">Q/A</p>
           <p className=" text-sm text-slate-200 mr-2">Privacy</p>
           <p className=" text-sm text-slate-200">Contact</p>
-        </div>
-        <div className=" w-1/2 flex justify-center items-center">
-          <p className=" text-sm text-slate-200">Cafe Bites | 2023</p>
         </div>
       </div>
     </div>
