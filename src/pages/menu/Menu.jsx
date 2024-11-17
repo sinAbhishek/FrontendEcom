d3ba3062210151f515b4c5f8f11253c233166130
sinAbhishek <singhabhshek979@gmail.com>
2024-11-17 19:55:17 +0530

    final

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -93,11 +93,6 @@ const Menu = () => {
     Food[0] && call();
   }, [Food]);
 
-  // const call = () => {
-  //   console.log(categoryDrink);
-  //   console.log(Food);
-  // };
-
   const filter = (e) => {
     e.preventDefault();
     setType(e.target.value);
