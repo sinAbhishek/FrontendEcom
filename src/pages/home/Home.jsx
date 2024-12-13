c105a3986f9751f41bbe8fff9df27b9e7f332b52
sinAbhishek <singhabhshek979@gmail.com>
2024-12-13 21:42:10 +0530

    final

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,5 @@
 import Book from "../../components/Book/Book.jsx";
 import Featured from "../../components/Featured/Featured.jsx";
-import Footer from "../../components/Footer/Footer.jsx";
-import Navbar from "../../components/navbar/Navbar.jsx";
 import Section from "../../components/Section/Section.jsx";
 import "./home.css";
 const Home = () => {
