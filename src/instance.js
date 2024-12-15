08b40a08e7be6c4184b0f544e84dd472cf2b0d7e
sinAbhishek <singhabhshek979@gmail.com>
2024-12-15 23:37:59 +0530

    fin

--- src/instance.js
+++ src/instance.js
@@ -1,9 +1,1 @@
-// import axios from "axios"
-// const URl="http://localhost:4000/api"
-// const post=axios.create({
-//     baseURL:URl,
-//     headers: {
-// 		Accept: 'application/json'
-//     }
-// });
-// export default post
+
